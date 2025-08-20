e3ad32f2b7fe6b0d8ba60322cec8dae8c7fc3846
raseward14 <raseward14@gmail.com>
2025-08-19 18:27:13 -0600

    initial commit header added and table week day header added day component day container and both component styles added

---
+++ src/app/ui/container/day-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'day-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-container.component.html',
+  styleUrl: './day-container.component.css'
+})
+export class DayContainer {}
