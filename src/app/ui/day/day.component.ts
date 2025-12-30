35daf7ae52c5a14cb3afdc9940d986151bf2c323
raseward14 <raseward14@gmail.com>
2025-12-30 16:56:09 -0700

    removed repetative week-day component and made the day component just a centered width moving the common css grid columns and rows to the day container both imported and used in the parent calendar

---
+++ src/app/ui/day/day.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-day",
+  imports: [],
+  templateUrl: "./day.component.html",
+  styleUrl: "./day.component.css",
+})
+export class Day {}
