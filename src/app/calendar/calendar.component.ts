cac5ea1321613d1afb8ea8764a134267bcf990f4
raseward14 <raseward14@gmail.com>
2025-12-30 16:59:23 -0700

    removed repetative week day container component moving the day container styles css grit col and rows to day container and day width and centered text to the day component both imported to calendar component where colors and appropriate heights are applied

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,33 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { WeekDay } from '../week-day/week-day.component';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 import { DayContainer } from "../ui/container/day-container.component";
+import { Day } from "../ui/day/day.component";
 
 @Component({
-  selector: 'app-calendar',
+  selector: "app-calendar",
   standalone: true,
-  imports: [WeekDay, CommonModule, DayContainer],
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  imports: [CommonModule, Day, DayContainer],
+  templateUrl: "./calendar.component.html",
+  styleUrls: ["./calendar.component.css"],
 })
-export class CalendarComponent implements OnInit{
+export class CalendarComponent implements OnInit {
   currentDate: Date = new Date();
   currentYear: number = this.currentDate.getFullYear();
-  currentMonth: number = this.currentDate.getMonth(); 
-  lastDayOfMonth: number = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
-  firstDayOfMonth: number = new Date(this.currentYear, this.currentMonth, 1).getUTCDay();
+  currentMonth: number = this.currentDate.getMonth();
+  lastDayOfMonth: number = new Date(
+    this.currentYear,
+    this.currentMonth + 1,
+    0
+  ).getDate();
+  firstDayOfMonth: number = new Date(
+    this.currentYear,
+    this.currentMonth,
+    1
+  ).getUTCDay();
   // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
-  dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   firstDayName: string = this.dayNames[this.firstDayOfMonth];
 
-  public cellsArray: number[] = Array(this.lastDayOfMonth).fill(0).map((x, i) => i + 1)
+  public cellsArray: number[] = Array(this.lastDayOfMonth)
+    .fill(0)
+    .map((x, i) => i + 1);
 
   ngOnInit(): void {
-    console.log('Calendar initialized with date:', this.currentDate);
+    console.log("Calendar initialized with date:", this.currentDate);
     console.log(`The current month has ${this.lastDayOfMonth} days.`);
     console.log(`The first day of the month is ${this.firstDayName}.`);
-    console.log('Cells Array:', this.cellsArray);
+    console.log("Cells Array:", this.cellsArray);
   }
 }
