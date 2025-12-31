f0f5be92b6bbead89ad40c852259f2e609a2c9c8
raseward14 <raseward14@gmail.com>
2025-12-31 13:12:22 -0700

    created setInterval and clearInterval in onDestroy to print current time, also compute all date and time values based off the initial signal for currentDate so only one value is updated every second, and all computed values change as a result

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy, signal, computed } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { DayContainer } from "../ui/container/day-container.component";
@@ -11,30 +11,31 @@ import { Day } from "../ui/day/day.component";
   templateUrl: "./calendar.component.html",
   styleUrls: ["./calendar.component.css"],
 })
-export class CalendarComponent implements OnInit {
-  currentDate: Date = new Date();
+export class CalendarComponent implements OnInit, OnDestroy {
+  // Use signal() to create a reactive date value
+  // Types are inferred
+  currentDate = signal(new Date());
 
-  currentYear: number = this.currentDate.getFullYear();
+  // Use computed() because this value depends on another signal
+  currentYear = computed(() => this.currentDate().getFullYear());
 
-  currentMonth: number = this.currentDate.getMonth();
+  currentMonth = computed(() => this.currentDate().getMonth());
 
-  currentDay: number = this.currentDate.getDate();
+  currentDay = computed(() => this.currentDate().getDate());
 
-  lastDayOfMonth: number = new Date(
-    this.currentYear,
-    this.currentMonth + 1,
-    0
-  ).getDate();
+  currentTime = computed(() => this.currentDate().getTime());
 
-  firstDayOfMonth: number = new Date(
-    this.currentYear,
-    this.currentMonth,
-    1
-  ).getUTCDay();
+  lastDayOfMonth = computed(() =>
+    new Date(this.currentYear(), this.currentMonth() + 1, 0).getDate()
+  );
+
+  firstDayOfMonth = computed(() =>
+    new Date(this.currentYear(), this.currentMonth(), 1).getUTCDay()
+  );
   // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
 
   // Generate an array of empty items to act as spacers
-  spacerDays = new Array(this.firstDayOfMonth);
+  spacerDays = computed(() => new Array(this.firstDayOfMonth()));
 
   dayNames = [
     "Sunday",
@@ -46,18 +47,37 @@ export class CalendarComponent implements OnInit {
     "Saturday",
   ];
 
-  firstDayName: string = this.dayNames[this.firstDayOfMonth];
+  firstDayName = computed(() => this.dayNames[this.firstDayOfMonth()]);
 
-  public cellsArray: number[] = Array(this.lastDayOfMonth)
-    .fill(0)
-    .map((x, i) => i + 1);
+  public cellsArray = computed(() =>
+    Array(this.lastDayOfMonth())
+      .fill(0)
+      .map((x, i) => i + 1)
+  );
+
+  private timerId: any;
 
   ngOnInit(): void {
-    console.log(`The first UTC day of the month is ${this.firstDayOfMonth}`);
-    console.log("Calendar initialized with date:", this.currentDate);
-    console.log(`The current month has ${this.lastDayOfMonth} days.`);
-    console.log(`The first day of the month is ${this.firstDayName}.`);
-    console.log("Cells Array:", this.cellsArray);
-    console.log(`The current day is: ${this.currentDay}`);
+    console.log(`The first UTC day of the month is ${this.firstDayOfMonth()}`);
+    console.log("Calendar initialized with date:", this.currentDate());
+    console.log(`The current month has ${this.lastDayOfMonth()} days.`);
+    console.log(`The first day of the month is ${this.firstDayName()}.`);
+    console.log("Cells Array:", this.cellsArray());
+    console.log(`The current day is: ${this.currentDay()}`);
+    console.log(`The current time is: ${this.currentTime()}`);
+
+    // Set up an interval timer to update the current date every second
+    // If I'm only updating once a day, this cold be set to 86400000 ms (24 hours)
+    // For demonstration, rendering a clock that updates every second in the UI
+    this.timerId = setInterval(() => {
+      this.currentDate.set(new Date());
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    // Clear the interval timer when the component is destroyed
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
   }
 }
