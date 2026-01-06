ee2b255e86d7ded764061728e4ba9a9f3359b17c
raseward14 <raseward14@gmail.com>
2026-01-05 18:04:57 -0700

    created forecast component

---
+++ src/app/forecast/forecast.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-forecast",
+  imports: [],
+  templateUrl: "./forecast.component.html",
+  styleUrl: "./forecast.component.css",
+})
+export class Forecast {}
