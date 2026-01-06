b3afe100687ad7766d9656f971998527c0996198
raseward14 <raseward14@gmail.com>
2026-01-05 18:08:13 -0700

    added openweather notes to forecast component

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -7,3 +7,8 @@ import { Component } from "@angular/core";
   styleUrl: "./forecast.component.css",
 })
 export class Forecast {}
+
+// - Your API key is 0f9dd32babe408905fb98d5398ec6131
+// - Please, use the endpoint api.openweathermap.org for your API calls
+// Useful links:
+// - API documentation https://openweathermap.org/api
